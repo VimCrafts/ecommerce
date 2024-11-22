150beed64794a3e0771d2fc35dfa8c55a06eb28b
mebz <zelalemmebea@gmail.com>
2024-11-22 15:51:07 +0300

    setup done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,29 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix: 'tw-',
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    colors: {
+      primary: {
+        pm1: '#000000',
       },
+      secondary: {
+        blue: '#377DFF',
+        green: '#38CB89',
+        orange: '#FFAB00',
+        red: '#FF5630',
+      },
+      neutral: {
+        n7: '#141718',
+        n6: '#232627',
+        n5: '#343839',
+        n4: '#6C7275',
+        n3: '#E8ECEF',
+        n2: '#F3F5F7',
+        n1: '#FEFEFE',
+      }
     },
+    extend: {},
   },
   plugins: [],
 } satisfies Config;
